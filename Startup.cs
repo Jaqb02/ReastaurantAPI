65b7fb9cbeb482d97963376b36d1b7c55aebdaec
Jakub Kurzawski <jakub.kurzawski.02@gmail.com>
2024-02-26 13:35:39 +0100

    case

--- Startup.cs
+++ Startup.cs
@@ -22,14 +22,12 @@ namespace ReastaurantAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IWeatherForecastService, WeatherForecastService>();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,8 +38,6 @@ namespace ReastaurantAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
